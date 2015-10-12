5c5464de5da3001ce7ce332a173e5b4fa645c36d
kohrVid <kohrVid@zoho.com>
2015-10-12 18:37:01 +0100

    Update made to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,13 +63,13 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
     config.action_mailer.default_url_options = { :host => 'kohrvid-env.elasticbeanstalk.com' }
-    Rails.application.routes.default_url_options[:host] = 'kohrvid-env.elasticbeanstalk.com'
+   # Rails.application.routes.default_url_options[:host] = 'kohrvid-env.elasticbeanstalk.com'
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = false
     config.action_mailer.default :charset => "utf-8"
 
-    conig.action_mailer.smtp_settings = {
+    config.action_mailer.smtp_settings = {
 	    address: "smtp.gmail.com",
 	    port: 587,
 	    domain: ENV["GMAIL_DOMAIN"],
