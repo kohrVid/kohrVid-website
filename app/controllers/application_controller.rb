5b57aabc174044c6f17df96244656e0af2e16dcd
kohrVid <kohrVid@zoho.com>
2015-10-13 10:17:40 +0100

    Still fixing mailer issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  	def authenticate
+	        authenticate_or_request_with_http_basic do |username, password|
+			username == ENV['GMAIL_USERNAME'] && password == ENV['GMAIL_PASSWORD']
+		end
+	end
+
 end
