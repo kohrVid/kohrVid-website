8599b3146e6c7ea25eaaee50321a0f131e47e40b
kohrVid <kohrVid@zoho.com>
2016-03-19 22:26:21 +0000

    Added Devise to the site

---
+++ spec/factories/clients.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+	factory :client do
+		client_name "Stranger'sCandy"
+		client_url "www.takesyouwhereyouwannabe.dep"
+		logo_url { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec", "public", "uploads", "client", "logo_url", "logo_image.jpg")) } 
+		image_url { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec", "public", "uploads", "client", "image_url", "bg_image.jpg")) } 
+		pdf { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec", "public", "uploads", "client", "pdf", "file.pdf")) } 
+		
+	end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+	factory :user do
+		name "MagicS"
+		email "nangioWah@premiergaou.ci"
+		password "onDit1st"
+		password_confirmation "onDit1st"
+		bio "Fool me once, shame on you. Fool me twice...!"
+		admin false
+	end
+end
