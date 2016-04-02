3164cd2ef4c8982b4b66855886296f7c62f899db
kohrVid <kohrVid@zoho.com>
2016-04-02 02:50:42 +0100

    Finished Blog Archive accordion menu

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,27 @@
 class Post < ActiveRecord::Base
-	
 	before_save do
 		published
 	end
-
+	extend FriendlyId
+	friendly_id :slug_candidates, use: [:slugged]
 	has_many :tags, through: :post_tags
 	has_many :post_tags
 	validates :title, presence: true, length: { maximum: 50 }, uniqueness: true
-	validates :body, presence: true, length: {minimum: 4, maximum: 20000}, uniqueness: true
+	validates :body, presence: true, length: { minimum: 4, maximum: 20000 }, uniqueness: true
 #	validates :post_tags, presence: true, associated: true
+
+	
+	def slug_candidates
+		[
+			:title,
+			[:title, :id]
+		]
+	end
 
 	def published
 		if (self.draft == false) && (self.published_at == nil)
 			self.published_at = Time.now
 		end
 	end
+	
 end
