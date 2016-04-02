efd93b8afb81d36d0b68f21f7f144efcebf7e250
kohrVid <kohrVid@zoho.com>
2016-04-02 13:13:17 +0100

    Fixed bug in Archives and added markdown support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 	ensure_security_headers
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include ApplicationHelper
   include NestHelper
 =begin
   	def authenticate
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
+require 'redcarpet'
+require 'coderay'
 module ApplicationHelper
+	class CodeRayify < Redcarpet::Render::HTML
+		def block_code(code, language)
+			CodeRay.scan(code, language).div
+		end
+	end
+
+	def markdown(text)
+		coderayified = CodeRayify.new(:filter_html => true, 
+			:hard_wrap => true)
+		options = {
+			:fenced_code_blocks => true,
+			:no_intra_emphasis => true,
+			:autolink => true,
+			:lax_html_blocks => true,
+			disable_indented_code_blocks: true
+		}
+		markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+		return markdown_to_html.render(text).html_safe
+		
+		#return markdown.render(text).html_safe
+	end
 end
