ded8d13c4da00534ac0995ee2d6ee3ef3813924f
kohrVid <kohrVid@zoho.com>
2017-04-28 21:29:18 +0100

    Added Rack::Deflater for scalability

--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,14 @@ Bundler.require(*Rails.groups)
 
 module KohrVid
   class Application < Rails::Application
-     config.time_zone = 'London'
-
+    config.time_zone = 'London'
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
     config.assets.initialized_on_precompile = true
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.middleware.use Rack::Deflater
     config.middleware.use Rack::Attack
-    config.action_controller.perform_caching = true 
+    config.action_controller.perform_caching = true
+    config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000' }
   end
 end
