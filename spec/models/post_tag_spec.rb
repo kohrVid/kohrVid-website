9010fd375daecd3a0c2d1ccebcb8e91c061872b9
kohrVid <kohrVid@zoho.com>
2018-12-30 14:55:40 +0000

    Fix PostTags spec and factory
    Refactored the factory so that it no longer needed to generate a new
    post or tag in order to function and have fixed tests that were broken
    as a result of this issue.

--- spec/models/post_tag_spec.rb
+++ spec/models/post_tag_spec.rb
@@ -1,34 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe PostTag, type: :model do
+  let(:valid_post_tag) { FactoryBot.create(:post_tag) }
+
   it "must have required fields or be invalid" do
-    pt = PostTag.new
-    expect(pt).to_not be_valid
+    expect(PostTag.new).to_not be_valid
   end
 
   it "creates a new post_tag relationship with valid attributes" do
-    expect{
-      PostTag.create(FactoryBot.attributes_for(:post_tag))
+    expect {
+      FactoryBot.build(:post_tag).save
     }.to change(PostTag, :count).by(1)
   end
 
   context "unique records" do
-    let(:valid_post) { FactoryBot.create(:post) }
-    let(:valid_tag) { FactoryBot.create(:tag) }
-
     it "must be unique" do
-      expect{
-        PostTag.create(FactoryBot.attributes_for(:post_tag, post_id: valid_post.id, tag_id: valid_tag.id))
-      }.to change(PostTag, :count).by(1)
-      expect{
-        PostTag.create(FactoryBot.attributes_for(:post_tag, post_id: valid_post.id, tag_id: valid_tag.id))
+      valid_post_tag
+
+      expect {
+        PostTag.create(FactoryBot.attributes_for(:post_tag))
       }.to_not change(PostTag, :count)
     end
   end
-  
+
   context "post_id" do
     it "must be present" do
-      expect{
+      expect {
         PostTag.create(FactoryBot.attributes_for(:post_tag, post_id: nil))
       }.to_not change(PostTag, :count)
     end
@@ -38,10 +35,10 @@ RSpec.describe PostTag, type: :model do
       expect(pt.errors[:post_id]).to_not be_nil
     end
   end
-  
+
   context "tag_id" do
     it "must be present" do
-      expect{
+      expect {
         PostTag.create(FactoryBot.attributes_for(:post_tag, tag_id: nil))
       }.to_not change(PostTag, :count)
     end
