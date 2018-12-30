9ddaefdf1855361c33a1d6b40be2fa1412c6aa92
kohrVid <kohrVid@zoho.com>
2018-12-30 03:30:31 +0000

    Fix Post specs and factory
    Placed the `title` and `body` attributes of the :post factory in a
    lambda to ensure that a unique paragraph and phrase is generated each
    time the factory is called. Have also fixed broken specs and the
    formatting in the Post model.

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :post do
-    title Faker::Lorem.words.join(" ")
-    body Faker::Lorem.paragraph
+    title { Faker::Lorem.words.join(" ") }
+    body { Faker::Lorem.paragraph }
     draft false
-    #slug "this-is-my-first-blog-post"
-    published_at Time.now#Time.new(2016, 03, 21, 11, 05, 00)
+    published_at Time.now
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  let(:post) { FactoryBot.create(:post) }
+
   it "must have required fields or be invalid" do
-    p = Post.new
-    expect(p).to_not be_valid
+    expect(Post.new).to_not be_valid
   end
 
   it "creates a new post with valid attributes" do
-    expect{
+    expect {
       Post.create(FactoryBot.attributes_for(:post))
     }.to change(Post, :count).by(1)
   end
 
   context "#title" do
     it "must be present" do
-      expect{
+      expect {
         Post.create(FactoryBot.attributes_for(:post, title: ""))
       }.to_not change(Post, :count)
     end
@@ -23,24 +24,26 @@ RSpec.describe Post, type: :model do
       p = Post.new
       expect(p.errors[:title]).to_not be_nil
     end
-    
+
     it "must be no more than 50 characters long" do
-      expect{
+      expect {
         Post.create(FactoryBot.attributes_for(:post, title: "m"*51))
       }.to_not change(Post, :count)
     end
-    
+
     it "must be unique" do
-      p = FactoryBot.create(:post)
-      expect{
-        Post.create(FactoryBot.attributes_for(:post, body: "New Body"))
+      post
+      expect {
+        Post.create(FactoryBot.attributes_for(
+          :post, title: post.title, body: "New Body"
+        ))
       }.to_not change(Post, :count)
     end
   end
 
   context "#body" do
     it "must be present" do
-      expect{
+      expect {
         Post.create(FactoryBot.attributes_for(:post, body: ""))
       }.to_not change(Post, :count)
     end
@@ -49,84 +52,97 @@ RSpec.describe Post, type: :model do
       p = Post.new
       expect(p.errors[:body]).to_not be_nil
     end
-    
+
     it "must be at least 4 characters long" do
-      expect{
+      expect {
         Post.create(FactoryBot.attributes_for(:post, body: "m"*3))
       }.to_not change(Post, :count)
     end
-    
+
     it "must be no more than 20000 characters long" do
-      expect{
+      expect {
         Post.create(FactoryBot.attributes_for(:post, body: "m"*20001))
       }.to_not change(Post, :count)
     end
-    
+
     it "must be unique" do
-      p = FactoryBot.create(:post)
-      expect{
-        Post.create(FactoryBot.attributes_for(:post, title: "New Title"))
+      post
+      expect {
+        Post.create(FactoryBot.attributes_for(
+          :post, title: "New Title", body: post.body
+        ))
       }.to_not change(Post, :count)
     end
   end
 
-  context ".draft" do
+  describe 'scopes' do
+    let(:post1) { FactoryBot.create(:post, draft: true) }
+    let(:post2) { FactoryBot.create(:post) }
+    let(:post3) { FactoryBot.create(:post, draft: true) }
+    let(:post4) { FactoryBot.create(:post) }
+
     before do
-      @one = FactoryBot.create(:post, draft: true)
-      @two = FactoryBot.create(:post)
-      @three = FactoryBot.create(:post, draft: true)
+      post1
+      post2
+      post3
+      post4
     end
 
-    it 'should contain all draft posts' do
-      expect(Post.drafts).to include(@one)
-      expect(Post.drafts).to include(@three)
+    context ".draft" do
+      subject { Post.drafts }
+
+      it 'should contain all draft posts' do
+        is_expected.to include(post1)
+        is_expected.to include(post3)
+      end
+
+      it 'should not contain any public posts' do
+        is_expected.to_not include(post2)
+        is_expected.to_not include(post4)
+      end
     end
 
-    it 'should not contain any public posts' do
-      expect(Post.drafts).to include(@one)
-      expect(Post.drafts).to_not include(@two)
-    end
-  end
+    context ".published" do
+      subject { Post.published }
 
-  context ".published" do
-    before do
-      @one = FactoryBot.create(:post)
-      @two = FactoryBot.create(:post, draft: true)
-      @three = FactoryBot.create(:post)
-    end
+      it 'should contain all published posts' do
+        is_expected.to include(post2)
+        is_expected.to include(post4)
+      end
 
-    it 'should contain all published posts' do
-      expect(Post.drafts).to include(@one)
-      expect(Post.drafts).to include(@three)
+      it 'should not contain any draft posts' do
+        is_expected.to_not include(post1)
+        is_expected.to_not include(post3)
+      end
     end
-
-    it 'should not contain any draft posts' do
-      expect(Post.drafts).to include(@one)
-      expect(Post.drafts).to_not include(@two)
-    end
-    #TODO write a test for the order of the posts
   end
 
   context "Tags" do
-    let(:post) { FactoryBot.create(:post) }
+    let(:tag1) { FactoryBot.create(:tag) }
+    let(:tag2) { FactoryBot.create(:tag, name: "Cats") }
+
+    let(:post_tag1) do
+      FactoryBot.build(:post_tag, post_id: post.id, tag_id: tag1.id)
+    end
+
+    let(:post_tag2) do
+      FactoryBot.build(:post_tag, post_id: post.id, tag_id: tag2.id)
+    end
+
     it "can have tags" do
       expect(Post.new).to respond_to(:tags)
     end
-    
+
     it "can have many tags" do
-      tag = FactoryBot.create(:tag)
-      tag2 = FactoryBot.create(:tag, name: "Cats")
-      p = FactoryBot.create(:post_tag, post_id: post.id, tag_id: tag.id)
-      p2 = FactoryBot.create(:post_tag, post_id: post.id, tag_id: tag2.id)
-      expect(p).to be_valid
-      expect(p2).to be_valid
+      expect(post_tag1).to be_valid
+      expect(post_tag2).to be_valid
     end
 
 =begin
     it "must have at least one tag" do
             p = Post.new
             expect(p).to_not be_valid
-            expect(p.errors[:tags]).to be_present 
+            expect(p.errors[:tags]).to be_present
     end
 =end
   end
