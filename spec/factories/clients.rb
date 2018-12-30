51b2bd81520e7ab2e0d03599fb3df817b42b7027
kohrVid <kohrVid@zoho.com>
2018-12-30 18:37:31 +0000

    Fix formatting in the clients factory
    Added some spacing and removed trailing white spaces so as to make this
    more readable.

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -2,9 +2,30 @@ FactoryBot.define do
   factory :client do
     client_name "Stranger'sCandy"
     client_url "www.takesyouwhereyouwannabe.dep"
-    logo_url { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec", "public", "uploads", "client", "logo_url", "logo_image.jpg")) } 
-    image_url { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec", "public", "uploads", "client", "image_url", "bg_image.jpg")) } 
-    pdf { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec", "public", "uploads", "client", "pdf", "file.pdf")) } 
-    
+
+    logo_url {
+      Rack::Test::UploadedFile.new(
+        File.join(
+          Rails.root, "spec", "public", "uploads", "client", "logo_url", "logo_image.jpg"
+        )
+      )
+    }
+
+    image_url {
+      Rack::Test::UploadedFile.new(
+        File.join(
+          Rails.root, "spec", "public", "uploads", "client", "image_url", "bg_image.jpg"
+        )
+      )
+    }
+
+    pdf {
+      Rack::Test::UploadedFile.new(
+        File.join(
+          Rails.root, "spec", "public", "uploads", "client", "pdf", "file.pdf"
+        )
+      )
+    }
+
   end
 end
