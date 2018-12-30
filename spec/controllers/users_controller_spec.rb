c644e8f4e78872be6175d28092d39840e2d3e0b6
kohrVid <kohrVid@zoho.com>
2018-12-30 01:14:54 +0000

    Fix UsersController spec
    Also refactored the spec for readability

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,34 +6,30 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #index" do
     before(:each) do
-      sign_in :user, user
-    #  allow(controller).to receive(:current_user).and_return(user)
+      sign_in user
+      get :index
     end
 
     it "populates an array of users" do
-      get :index
       expect(assigns(:users)).to eq([user, admin])
     end
 
     it "returns http success" do
-      get :index
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #show" do
     before(:each) do
-      sign_in :user, user
-      #allow(controller).to receive(:current_user).and_return(user)
+      sign_in user
+      get(:show, params: { id: user.id })
     end
     
     it "assigns requested user to @user" do
-      get(:show, id: user.id)
       expect(assigns(:user)).to eq(user)
     end
 
     it "returns http success" do
-      get(:show, id: user.id)
       expect(response).to have_http_status(:success)
     end
   end
@@ -41,144 +37,173 @@ RSpec.describe UsersController, type: :controller do
   describe "GET #new" do
     before(:each) do
       sign_in :user, admin
-      #allow(controller).to receive(:current_user).and_return(admin)
+      get(:new)
     end
 
     it "returns http success" do
-      get(:new)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "POST create" do
     before(:each) do
-      sign_in :user, admin
-      #allow(controller).to receive(:current_user).and_return(admin)
+      sign_in admin
     end
 
     context "with valid attributes" do
+      subject(:post_attributes) do
+        post :create, params: {
+          user: FactoryBot.attributes_for(:user, :reader)
+        }
+      end
+
       it "creates a new user" do
-        expect{
-          post :create, user: FactoryBot.attributes_for(:user, :reader)
-        }.to change(User, :count).by(1)
+        expect { post_attributes }.to change(User, :count).by(1)
       end
 
       it "redirects to the new user" do
-        post :create, user: FactoryBot.attributes_for(:user, :reader)
-        expect(response).to redirect_to User.last
+        is_expected.to redirect_to User.last
       end
 
       it "displays the correct flash message on redirect" do
-        post :create, user: FactoryBot.attributes_for(:user, :reader)
+        subject
         expect(flash[:success]).to have_content("User was created successfully.")
       end
     end
 
     context "with invalid attributes" do
+      subject(:post_attributes) do
+        post :create, params: {
+          user: FactoryBot.attributes_for(:user, :reader, name: "")
+        }
+      end
+
       it "doesn't save the new user" do
-        expect{
-          post :create, user: FactoryBot.attributes_for(:user, :reader, name: "")
-        }.to_not change(User, :count)
+        expect { post_attributes }.to_not change(User, :count)
       end
 
       it "re-renders the new method" do
-        post :create, user: FactoryBot.attributes_for(:user, :reader, name: "")
-        expect(response).to render_template :new
+        is_expected.to render_template :new
       end
 
       it "displays the correct flash message on redirect" do
-        post :create, user: FactoryBot.attributes_for(:user, :reader, name: "")
-        expect(flash[:error]).to have_content("An error has prevented this account from being saved")
+        subject
+        expect(flash[:error]).to have_content(
+          "An error has prevented this account from being saved"
+        )
       end
     end
   end
 
   describe "GET #edit" do
-    before(:each) do
-      sign_in :user, admin
-      @user = user
+    before do
+      sign_in admin
+      get(:edit, params: { id: user.id })
     end
 
     it "assigns requested user to @user" do
-      get(:edit, id: user.id)
       expect(assigns(:user)).to eq(user)
     end
 
     it "returns http success" do
-      get(:edit, id: user.id)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "PUT update" do
-    before(:each) do
-      sign_in :user, admin
-      @user = user
+    before do
+      sign_in admin
     end
 
     context "with valid attributes" do
-      it "locates the requested @user" do
-        put :update, id: @user.id, user: FactoryBot.attributes_for(:user, :reader)
-        expect(assigns(:user)).to eq(user)
+      subject(:put_attributes) do
+        put :update, params: {
+          id: user.id,
+          user: FactoryBot.attributes_for(
+            :user,
+            :reader,
+            name: 'Raine',
+            bio: 'Raine is from Cote d\'Ivoire'
+          )
+        }
       end
 
-      it "changes @user's attributes" do
-        put :update, id: @user.id, user: FactoryBot.attributes_for(:user, :reader, name: "Services", bio: "These are the services we provide.")
-        user.reload
-        expect(@user.name).to eq("Services")
-        expect(@user.bio).to eq("These are the services we provide.")
+      it "changes @user's name" do
+        expect { put_attributes }
+          .to change { User.find(user.id).name }
+          .from(user.name).to('Raine')
+      end
+
+      it "changes @user's bio" do
+        expect { put_attributes }
+          .to change { User.find(user.id).bio }
+          .from(user.bio).to( 'Raine is from Cote d\'Ivoire')
       end
 
       it "redirects to the updated user" do
-        put :update, id: @user.id, user: FactoryBot.attributes_for(:user, :reader)
-        expect(response).to redirect_to user
+        is_expected.to redirect_to user
       end
 
       it "displays the correct flash message on redirect" do
-        put :update, id: @user.id, user: FactoryBot.attributes_for(:user, :reader)
+        subject
         expect(flash[:success]).to have_content("Profile updated.")
       end
     end
 
     context "with invalid attributes" do
-      it "shouldn't change @user's attributes" do
-        put :update, id: @user.id, user: FactoryBot.attributes_for(:user, :reader, name: "", bio: "r")
-        @user.reload
-        expect(@user.name).to_not eq("")
-        expect(@user.bio).to_not eq("r")
+      subject(:put_attributes) do
+        put :update, params: {
+          id: user.id,
+          user: FactoryBot.attributes_for(
+            :user,
+            :reader,
+            name: "",
+            bio: "r"
+          )
+        }
+      end
+
+      it "does not change @user's name" do
+        expect { put_attributes }
+          .to_not change { User.find(user.id).name }
+      end
+
+      it "does not change @user's bio" do
+        expect { put_attributes }
+          .to_not change { User.find(user.id).bio }
       end
 
       it "renders the edit user" do
-        put :update, id: @user.id, user: FactoryBot.attributes_for(:user, :reader, name: "", bio: "Raine is from Cote d'Ivoire")
-        expect(response).to render_template :edit
+        is_expected.to render_template :edit
       end
 
       it "displays the correct flash message on redirect" do
-        put :update, id: @user.id, user: FactoryBot.attributes_for(:user, :reader, name: "", bio: "r")
+        subject
         expect(flash[:error]).to have_content("Unable to update profile.")
       end
     end
   end
 
   describe "DELETE destroy" do
+    subject(:delete_user) do
+      delete :destroy, params: { id: user.id }
+    end
+
     before(:each) do
-      sign_in :user, admin
-      @user = user
+      sign_in admin
+      user
     end
 
     it "deletes the user" do
-      expect{
-        delete :destroy, id: @user.id
-      }.to change(User, :count).by(-1)
+      expect { delete_user }.to change(User, :count).by(-1)
     end
 
     it "redirects to the users#index" do
-      delete :destroy, id: @user.id
-      expect(response).to redirect_to users_path
+      is_expected.to redirect_to users_path
     end
 
     it "displays the correct flash message on redirect" do
-      delete :destroy, id: @user.id
+      subject
       expect(flash[:success]).to have_content("User deleted.")
     end
   end
