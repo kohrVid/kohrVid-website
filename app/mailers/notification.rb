e0ea1994e8a6f9331b24dadece969fefa17a6c55
kohrVid <kohrVid@zoho.com>
2018-12-31 05:06:18 +0000

    Changed instance variable in Notification mailer
    Changed the instance variable listed in the Notificiation mailer to
    `@new_comment` as I was worried that using the same instance variable as
    the CommentsController could get confusing. Have also simplified the
    code used to generate the email subject

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -1,8 +1,9 @@
 class Notification < ApplicationMailer
   def new_comment_admin_notification(comment)
-    @comment = comment
+    @new_comment = comment
     mail from: "noreply@kohrVid.com"
+
     mail to: ENV['GMAIL_USERNAME'],
-      subject: "New comment posted under '#{Post.find(@comment.post_id).title }'"
+      subject: "New comment posted under '#{@new_comment.post.title }'"
   end
 end
