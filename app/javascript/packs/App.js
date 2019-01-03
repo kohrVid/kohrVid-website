1c93e4c7a1bd9347de63557d71b39407241c5ce6
kohrVid <kohrVid@zoho.com>
2019-01-03 02:31:05 +0000

    Refactored the Portfolio page
    The portfolio page now contains the old links to Clients and Projects
    but these now open the relevant React containers on the same page.

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
-import ClientsContainer from './components/ClientsContainer'
+import ClientsContainer from './components/ClientsContainer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false
+    };
+    this.portfolioContainerOpen = this.portfolioContainerOpen.bind(this);
+    this.clientContainerOpen = this.clientContainerOpen.bind(this);
+  }
+
+  clientContainerOpen() {
+    ReactDOM.render(<ClientsContainer />, document.getElementById('Portfolio'));
+  }
+
+  portfolioContainerOpen() {
+    ReactDOM.render(<PortfolioContainer />, document.getElementById('Portfolio'));
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" id="Portfolio">
         <div className="App-header">
           <h1>
-            Clients
+            Portfolio
           </h1>
-          <ClientsContainer />
         </div>
+        <ul className="diamond-list">
+          <li className="padded-li">
+            <a href="#" onClick={this.portfolioContainerOpen}>Previous Projects</a>
+          </li>
+          <li className="padded-li">
+          <a href="#" onClick={this.clientContainerOpen}>Clients</a></li>
+        </ul>
       </div>
     );
   }
