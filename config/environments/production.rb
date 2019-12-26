3d2ecc80e737aea2fb4fbba5924b15cef5bc3d51
kohrVid <kohrVid@zoho.com>
2019-12-26 23:50:58 +0000

    Switching off asset compile in production
    This should be set to false in production as switching this on is
    slower (and results in all requests for assets being passed to
    sprockets, &c.). Hopefully this won't break with Rails 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.assets.compile = true
+  config.assets.compile = false
 
   config.assets.digest = true
 
