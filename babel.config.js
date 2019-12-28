e78cca0fc03b0709640b6deeaa9e187c0c944b5b
kohrVid <kohrVid@zoho.com>
2019-12-28 13:38:27 +0000

    Fix "Cannot find module 'babel-preset-env'" error?

--- babel.config.js
+++ babel.config.js
@@ -20,6 +20,7 @@ module.exports = function(api) {
       isTestEnv && [
         require('@babel/preset-env').default,
         {
+          modules: "commonjs",
           targets: {
             node: 'current'
           }
