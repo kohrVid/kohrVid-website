01f83417532fdd9c36e4ef84d239494fc2655463
kohrVid <kohrVid@zoho.com>
2019-12-29 04:05:42 +0000

    Reorganise production.rb
    Still trying to fix issues with precompiled assets in Rails 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,48 +1,7 @@
 Rails.application.configure do
-  config.cache_classes = true
-
-  config.eager_load = true
-
-  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # config.action_dispatch.rack_cache = true
-
-  config.serve_static_files = true
-  config.public_file_server.enabled = true
-
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  config.assets.compile = false
-
-  config.assets.digest = true
-
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.force_ssl = false
-=begin
-  config.ssl_options = {
-    redirect: {
-      status: 307, port: 81
-    },
-    secure_cookies: true,
-    hsts: {
-      preload: true
-    }
-  }
-=end
-
-  config.log_level = :debug
-
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # config.cache_store = :mem_cache_store
-
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   config.action_mailer.default_url_options = {
@@ -64,11 +23,53 @@ Rails.application.configure do
     password: ENV["GMAIL_PASSWORD"]
   }
 
-  config.i18n.fallbacks = [I18n.default_locale]
 
   config.active_support.deprecation = :notify
 
+  config.active_record.dump_schema_after_migration = false
+
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
+  config.assets.compile = false
+
+  config.assets.digest = true
+
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+  config.cache_classes = true
+  config.consider_all_requests_local       = false
+
+  config.eager_load = true
+  config.force_ssl = false
+
+  config.i18n.fallbacks = [I18n.default_locale]
+
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+
   config.log_formatter = ::Logger::Formatter.new
 
-  config.active_record.dump_schema_after_migration = false
+  config.log_level = :debug
+
+  # config.log_tags = [ :subdomain, :uuid ]
+
+  config.public_file_server.enabled = true
+  config.serve_static_files = true
+
+=begin
+  config.ssl_options = {
+    redirect: {
+      status: 307, port: 81
+    },
+    secure_cookies: true,
+    hsts: {
+      preload: true
+    }
+  }
+=end
+  # config.cache_store = :mem_cache_store
+
 end
