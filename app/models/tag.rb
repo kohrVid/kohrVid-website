eddce2bf109d11f30dcb0b21ddfe5b632fbfe5dc
kohrVid <kohrVid@zoho.com>
2019-12-30 23:19:39 +0000

    Fix Tag relationship methods
    Have fixed the class methods in the Tag class so as to resolve a bug
    that appears when an existing blog post is updated (this started to
    appear after the RoR 6 upgrade). This should also greatly simplify the
    way in which these methods behaved. In particular, I've removed a method
    that checked for duplicates in the post_tags table as it's already
    possible to validate for uniqueness on the the two coloumns in the
    PostTag class.
    Have added tests for the relevant methods as the test coverage wasn't
    great for this class before this commit.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,56 +3,50 @@ class Tag < ActiveRecord::Base
   has_many :posts, -> { distinct }, through: :post_tags
 
   validates :name, presence: true, length: { maximum: 15 }, uniqueness: true
-  
+
+
+  # Note, the class methods are quite generic as the `tags` table could
+  # theoretically be associated with other tables in the future 
   def self.tag_relationship(object)
-    unless object.tag_list.nil?
-      list = object.tag_list.split(",").collect{|i| i.gsub!(/^\s*/, "")}.reject(&:blank?)
-      add_tag_relationship(object, list)
-      check_tag_relationship(object, list)
-      check_for_duplicates(object, list)
+    unless object.tag_list.blank?
+      tag_list = clean_tag_names_list(object.tag_list)
+      current_tag_relationships = add_tag_relationships(object, tag_list)
+      remove_old_tag_relationships(object, current_tag_relationships)
     end
   end
 
-  def self.add_tag_relationship(object, list)
-    through_table, model_column = handle_through_table(object.class)
-    list.each do |tag_name|
-      if Tag.find_by(name: tag_name.to_s).nil?
-        tag = object.tags.create(name: tag_name.to_s)
-      else
-        tag = Tag.find_by(name: tag_name.to_s)
-        through_table.create!(
-          model_column => object.id, tag_id: tag.id
-        )
-      end
+  def self.add_tag_relationships(object, tag_list)
+    through_table, column = handle_through_table(object.class)
+    current_tag_relationships = []
+
+    tag_list.each do |tag_name|
+      tag = Tag.find_or_create_by(name: tag_name)
+      
+      tag_relationship = through_table.find_or_create_by(
+        column => object.id, tag_id: tag.id
+      )
+      
+      current_tag_relationships << tag_relationship
     end
+
+    current_tag_relationships
   end
 
-  def self.check_tag_relationship(object, list)
-    through_table, model_column = handle_through_table(object.class)
-    relationships = through_table.where(model_column => object.id)
-    relationships.each do |row|
-      unless list.include? Tag.find(row.tag_id).name
-        row.destroy!
-      end
-    end
+  def self.remove_old_tag_relationships(object, current_tag_relationships)
+    through_table, column = handle_through_table(object.class)
+    old_relationships = through_table.where(column => object.id)
+
+    (old_relationships - current_tag_relationships).map(&:destroy!)
   end
 
-  def self.check_for_duplicates(object, list)
-    through_table, model_column = handle_through_table(object.class)
-    list.each do |tag_name|
-      relationships = through_table.where(
-        model_column => object.id,
-        tag_id: Tag.find_by(name: tag_name.to_s).id
-      ).order(:created_at)
-      unless relationships.count <= 1
-        relationships.all[1..-1].each do |duplicate|
-          duplicate.destroy!
-        end
-      end
-    end
+  def self.handle_through_table(klass)
+    ["#{ klass }Tag".constantize, "#{ klass.name.underscore }_id"]
   end
 
-  def self.handle_through_table(model)
-    ["#{ model }Tag".constantize, "#{ model.name.underscore }_id"]
+  def self.clean_tag_names_list(tag_names)
+    tag_names.downcase.split(",")
+      .collect { |i| i.gsub!(/^\s*/, "") }
+      .reject(&:blank?)
+      .map(&:to_s)
   end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
+  let(:object) { FactoryBot.create(:post, tag_list: "ruby, scala") }
+
   it "must have required fields or be invalid" do
     t = Tag.new
     expect(t).to_not be_valid
@@ -44,4 +46,102 @@ RSpec.describe Tag, type: :model do
       expect(Tag.new).to respond_to(:posts)
     end
   end
+
+  context "tag relationships" do
+    let(:tag_relationships) do
+      Tag.add_tag_relationships(object, ["ruby", "scala", "go"])
+    end
+
+    context ".add_tag_relationships" do
+      it "should add any new tags" do
+        tag_relationships
+
+        [ "ruby", "scala", "go" ].each do |tag_name|
+          expect(
+            Tag.where(name: tag_name).to_a
+          ).to_not be_empty 
+        end
+      end
+
+      it "should add new rows in the relevant through table" do
+        tag_relationships.map(&:tag_id).each do |tag_id|
+          expect(
+            PostTag.where(post_id: object.id, tag_id: tag_id).to_a
+          ).to_not be_empty
+        end
+      end
+    end
+
+    context ".remove_old_tag_relationships" do
+      let(:tags) { Tag.where("name in (?)", ["ruby", "scala"]) }
+      let(:current_tag_relationships) do
+        Tag.add_tag_relationships(object, ["ruby", "scala"])
+      end
+
+      before(:each) do
+        tag_relationships
+        Tag.remove_old_tag_relationships(object, current_tag_relationships)
+      end
+
+      it "should delete any old relationships" do
+        tag_id = Tag.where(name: "go").first.id
+    
+        expect(
+          PostTag.where(post_id: object.id, tag_id: tag_id).to_a
+        ).to be_empty
+      end
+
+      it "should not delete current relationships" do
+        tags.map(&:id).each do |tag_id|
+          expect(
+            PostTag.where(post_id: object.id, tag_id: tag_id).to_a
+          ).to_not be_empty
+        end
+      end
+    end
+
+    context "tag_relationship" do
+      before(:each) do
+        tag_relationships
+      end
+
+      it "should not create duplicate relationships" do
+        Tag.tag_relationship(object)
+        current_tag_ids = Tag.where("name in (?)", ["ruby", "scala"]).map(&:id)
+
+        current_tag_ids.each do |tag_id|
+          expect(
+            PostTag.where(post_id: object.id, tag_id: tag_id).count
+          ).to eq 1
+        end
+      end
+    end
+
+    context ".handle_through_table" do
+      it "should return the right class name for the through table" do
+        expect(Tag.handle_through_table(Post).first).to eq(PostTag)
+      end
+
+      it "should return the right column name for the through table" do
+        expect(Tag.handle_through_table(Post).second).to eq("post_id")
+      end
+    end
+
+    context ".clean_tag_names_list" do
+      it "should only return lower case tag names" do
+        ["ruby", "go", "scala"].each do |tag_name|
+          expect(
+            Tag.clean_tag_names_list("RUBY, gO, Scala")
+          ).to include(tag_name)
+        end
+      end
+
+      it "should not return blank tags" do
+        tags = Tag.clean_tag_names_list(" , RUBY,,")
+        expect(tags.count).to eq 1
+        expect(tags).to include("ruby")
+      end
+    end
+  end
+
 end
