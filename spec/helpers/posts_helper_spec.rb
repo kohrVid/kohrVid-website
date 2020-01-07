ef383d18b11236b86ded2fbbf984179888357135
kohrVid <kohrVid@zoho.com>
2020-01-07 18:31:13 +0000

    Fix issues with post truncation
    Truncated posts with unclosed tags will mess up the HTML on the
    posts#index page (this is currently happening in production where a
    number of posts are now bold when they shouldn't be). This change should
    resolve this issue.

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,15 +1,30 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the PostsHelper. For example:
-#
-# describe PostsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe PostsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:html_content) { "<div>#{Faker::Lorem.sentence}</div>" }
+  let(:content) { ActionText::Content.new(html_content) }
+  let(:rich_text_body) { ActionText::RichText.new(body: content) }
+
+  context "truncate_body" do
+    it "should not attempt to truncate a rich text body if it is null" do
+      subject = truncate_body(nil)
+      expect(subject).to eq nil
+    end
+
+    it "should truncate rich text based on the length attribute" do
+      subject = truncate_body(rich_text_body, 10)
+
+      expect(subject.split("...").first.length).to eq 10
+    end
+
+    context "unclosed tags" do
+      let(:html_content) { "<b>memwew</b>" }
+
+      it "should close any open html tags by default" do
+        subject = truncate_body(rich_text_body, 6)
+        expect(subject).to_not eq "<b>mem..."
+        expect(subject).to eq "<b>mem...</b>"
+      end
+    end
+  end
 end
