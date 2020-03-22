5139be6af75197cdcef72a01a540636704a4a9a0
kohrVid <kohrVid@zoho.com>
2020-03-22 14:57:17 +0000

    Remove actiontext from package.json
    The ActiveStorage package is still required but it should be safe to
    remove the ActionText and Trix packages from the node configuration.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-//require("@rails/actiontext")
 require("@rails/activestorage").start()
 import "../css/application.css"
 import "./quill-editor.js"
