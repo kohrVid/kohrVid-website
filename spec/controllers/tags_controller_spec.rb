a1da1b324c603a0738edb7320d483a1eedab69a7
Jessica Été <kohrVid@zoho.com>
2020-08-02 18:08:05 +0100

    Fix tests for Tags controller
    Tests now check the permissions are set up correctly. Have also fixed a
    condition in the tags view that I believe would have allowed non-admins
    to view links to actions that they don't have access to.

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe TagsController, type: :controller do
   let(:valid_tag) { FactoryBot.create(:tag) }
   let(:admin) { FactoryBot.create(:user, :admin) }
+  let(:user) { FactoryBot.create(:user, :reader_one) }
 
   describe "GET #index" do
     it "populates an array of tags" do
@@ -31,128 +32,215 @@ RSpec.describe TagsController, type: :controller do
   end
 
   describe "GET #new" do
-    before(:each) do
-      sign_in admin, scope: :user
-      get(:new)
+    subject { get(:new) }
+
+    context "as a signed-out user" do
+      it "redirects to the new tag" do
+        expect { subject }.to raise_error
+      end
     end
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
+    context "as a non-admin user" do
+      before(:each) do
+        sign_in user, scope: :user
+      end
+
+      it "redirects to the new tag" do
+        expect { subject }.to raise_error
+      end
+    end
+
+    context "as an admin" do
+      before(:each) do
+        sign_in admin, scope: :user
+        subject
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
   describe "POST create" do
-    before(:each) do
-      sign_in admin, scope: :user
+    subject(:post_attributes) do
+      post :create, params: {
+        tag: FactoryBot.attributes_for(:tag)
+      }
     end
 
-    context "with valid attributes" do
-      subject(:post_attributes) do
-        post :create, params: {
-          tag: FactoryBot.attributes_for(:tag)
-        }
+    context "as a signed-out user" do
+      it "redirects to the new tag" do
+        expect { post_attributes }.to raise_error
       end
+    end
 
-      it "creates a new post" do
-        expect { post_attributes }.to change(Tag, :count).by(1)
+    context "as a non-admin user" do
+      before(:each) do
+        sign_in user, scope: :user
       end
 
       it "redirects to the new tag" do
-        is_expected.to redirect_to Tag.last
-      end
-
-      it "displays the correct flash message on redirect" do
-        subject
-        expect(flash[:success]).to have_content("Tag was created successfully.")
+        expect { post_attributes }.to raise_error
       end
     end
 
-    context "with invalid attributes" do
-      subject(:post_attributes) do
-        post :create, params: {
-          tag: FactoryBot.attributes_for(:tag, name: '')
-        }
+    context "as an admin" do
+      before(:each) do
+        sign_in admin, scope: :user
       end
 
-      it "doesn't save the new tag" do
-        expect{ post_attributes }.to_not change(Tag, :count)
+      context "with valid attributes" do
+        subject(:post_attributes) do
+          post :create, params: {
+            tag: FactoryBot.attributes_for(:tag)
+          }
+        end
+
+        it "creates a new post" do
+          expect { post_attributes }.to change(Tag, :count).by(1)
+        end
+
+        it "redirects to the new tag" do
+          is_expected.to redirect_to Tag.last
+        end
+
+        it "displays the correct flash message on redirect" do
+          subject
+
+          expect(flash[:success]).to have_content(
+            "Tag was created successfully."
+          )
+        end
       end
 
-      it "re-renders the new method" do
-        is_expected.to render_template :new
-      end
+      context "with invalid attributes" do
+        subject(:post_attributes) do
+          post :create, params: {
+            tag: FactoryBot.attributes_for(:tag, name: '')
+          }
+        end
 
-      it "displays the correct flash message on redirect" do
-        subject
-        expect(flash[:error])
-          .to have_content("An error has prevented this tag from being saved")
+        it "doesn't save the new tag" do
+          expect{ post_attributes }.to_not change(Tag, :count)
+        end
+
+        it "re-renders the new method" do
+          is_expected.to render_template :new
+        end
+
+        it "displays the correct flash message on redirect" do
+          subject
+          expect(flash[:error])
+            .to have_content("An error has prevented this tag from being saved")
+        end
       end
     end
   end
 
   describe "GET #edit" do
-    before(:each) do
-      sign_in admin, scope: :user
+    subject do
       get(:edit, params: { id: valid_tag.id })
     end
 
-    it "assigns requested post to @tag" do
-      expect(assigns(:tag)).to eq(valid_tag)
+    context "as a signed-out user" do
+      it "redirects to the new tag" do
+        expect { subject }.to raise_error
+      end
     end
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
+    context "as a non-admin user" do
+      before(:each) do
+        sign_in user, scope: :user
+      end
+
+      it "redirects to the new tag" do
+        expect { subject }.to raise_error
+      end
+    end
+
+    context "as an admin" do
+      before(:each) do
+        sign_in admin, scope: :user
+        subject
+      end
+
+      it "assigns requested post to @tag" do
+        expect(assigns(:tag)).to eq(valid_tag)
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
   describe "PUT update" do
-    before(:each) do
-      sign_in admin, scope: :user
+    subject(:put_attributes) do
+      put :update, params: {
+        id: valid_tag.id, tag: FactoryBot.attributes_for(:tag, name: 'Food')
+      }
     end
 
-    context "with valid attributes" do
-      subject(:put_attributes) do
-        put :update, params: {
-          id: valid_tag.id, tag: FactoryBot.attributes_for(:tag, name: 'Food')
-        }
-      end
-
-      it "changes @tag's attributes" do
-        expect { put_attributes }
-          .to change { Tag.find(valid_tag.id).name }
-          .from(valid_tag.name).to('Food')
-      end
-
-      it "redirects to the updated post" do
-        is_expected.to redirect_to valid_tag
-      end
-
-      it "displays the correct flash message on redirect" do
-        subject
-        expect(flash[:success]).to have_content("Tag updated.")
+    context "as a signed-out user" do
+      it "redirects to the new tag" do
+        expect { put_attributes }.to raise_error
       end
     end
 
-    context "with invalid attributes" do
-      subject(:put_attributes) do
-        put :update, params: {
-          id: valid_tag.id, tag: FactoryBot.attributes_for(:tag, name: '')
-        }
+    context "as a non-admin user" do
+      before(:each) do
+        sign_in user, scope: :user
       end
 
-      it "shouldn't change @tag's attributes" do
-        valid_tag.reload
-        expect { put_attributes }
-          .to_not change { Tag.find(valid_tag.id).name }
+      it "redirects to the new tag" do
+        expect { put_attributes }.to raise_error
+      end
+    end
+
+    context "as an admin" do
+      before(:each) do
+        sign_in admin, scope: :user
       end
 
-      it "renders the edit post" do
-        is_expected.to render_template :edit
+      context "with valid attributes" do
+        it "changes @tag's attributes" do
+          expect { put_attributes }
+            .to change { Tag.find(valid_tag.id).name }
+            .from(valid_tag.name).to('Food')
+        end
+
+        it "redirects to the updated post" do
+          is_expected.to redirect_to valid_tag
+        end
+
+        it "displays the correct flash message on redirect" do
+          subject
+          expect(flash[:success]).to have_content("Tag updated.")
+        end
       end
 
-      it "displays the correct flash message on redirect" do
-        subject
-        expect(flash[:error]).to have_content("Unable to update tag.")
+      context "with invalid attributes" do
+        subject(:put_attributes) do
+          put :update, params: {
+            id: valid_tag.id, tag: FactoryBot.attributes_for(:tag, name: '')
+          }
+        end
+
+        it "shouldn't change @tag's attributes" do
+          valid_tag.reload
+          expect { put_attributes }
+            .to_not change { Tag.find(valid_tag.id).name }
+        end
+
+        it "renders the edit post" do
+          is_expected.to render_template :edit
+        end
+
+        it "displays the correct flash message on redirect" do
+          subject
+          expect(flash[:error]).to have_content("Unable to update tag.")
+        end
       end
     end
   end
@@ -162,22 +250,40 @@ RSpec.describe TagsController, type: :controller do
       delete :destroy, params: { id: valid_tag.id }
     end
 
-    before(:each) do
-      sign_in admin, scope: :user
-      valid_tag
+    context "as a signed-out user" do
+      it "redirects to the new tag" do
+        expect { delete_tag }.to raise_error
+      end
     end
 
-    it "deletes the post" do
-      expect { delete_tag }.to change(Tag, :count).by(-1)
+    context "as a non-admin user" do
+      before(:each) do
+        sign_in user, scope: :user
+      end
+
+      it "redirects to the new tag" do
+        expect { delete_tag }.to raise_error
+      end
     end
 
-    it "redirects to the tags#index" do
-      is_expected.to redirect_to tags_path
-    end
+    context "as an admin" do
+      before(:each) do
+        sign_in admin, scope: :user
+        valid_tag
+      end
 
-    it "displays the correct flash message on redirect" do
-      subject
-      expect(flash[:success]).to have_content("Tag deleted.")
+      it "deletes the post" do
+        expect { delete_tag }.to change(Tag, :count).by(-1)
+      end
+
+      it "redirects to the tags#index" do
+        is_expected.to redirect_to tags_path
+      end
+
+      it "displays the correct flash message on redirect" do
+        subject
+        expect(flash[:success]).to have_content("Tag deleted.")
+      end
     end
   end
 end
