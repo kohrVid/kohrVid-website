2e420e8579ae14610862a1314ca71d4a807883f4
Jessica Été <kohrVid@zoho.com>
2020-08-02 11:31:49 +0100

    Limit image uploads
    I feel a little nervous about letting anonymous users post images so I'm
    disabling this feature for them. Anyone uplading images in the blog will
    have to log in first from now on (sorry!).

--- app/javascript/packs/quill-editor.js
+++ app/javascript/packs/quill-editor.js
@@ -7,47 +7,86 @@ Quill.register('modules/imageResize', ImageResize);
 
 const MAX_FILE_SIZE = 1000000
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  var quill = new Quill('#editor-container', {
-    modules: {
-      toolbar: [
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
-        [{ color: [] }],
-        [{ size: [] }],
-        [
-          'bold', 'italic', 'underline', 'strike',
-          { 'script': 'super'},
-          { 'script': 'sub' },
-          'code', 'link'
-        ],
-        ['blockquote', 'code-block', 'image'],
-        [{ list: 'ordered' }, { list: 'bullet' }],
-        [{ align: ['center', 'right', 'justify', false] }],
-        [{ indent: '-1'}, { indent: '+1' }]
+const basicToolbar = (document) => ({
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ color: [] }],
+      [{ size: [] }],
+      [
+        'bold', 'italic', 'underline', 'strike',
+        { 'script': 'super'},
+        { 'script': 'sub' },
+        'code', 'link'
       ],
-      imageResize: {
-        displaySize: true,
-        displayStyles: {
-          backgroundColor: 'black',
-          border: 'none',
-          color: 'white'
-        },
-        modules: [ 'Resize', 'DisplaySize', 'Toolbar' ]
-      }
-    },
-    value: document.querySelector('input[class=rich-text-content]').value,
-    theme: 'snow'
-  });
+      ['blockquote', 'code-block'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ align: ['center', 'right', 'justify', false] }],
+      [{ indent: '-1'}, { indent: '+1' }]
+    ],
+  },
+  value: document.querySelector('input[class=rich-text-content]').value,
+  theme: 'snow',
+  formats: [
+    'background', 'bold', 'color', 'font', 'code', 'italic', 'link', 'size',
+    'strike', 'script', 'underline', 'blockquote', 'header', 'indent', 'list',
+    'align', 'direction', 'code-block', 'formula']
+});
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  let quill;
+
+  if (document.getElementById("editor-container-strict") !== null) {
+    quill = renderBasicEditor(basicToolbar(document))
+  } else {
+    quill =  renderExtendedEditor(basicToolbar(document))
+  }
 
   document.querySelector('form').onsubmit = function () {
     var body = document.querySelector('input[class=rich-text-content]');
     body.value = quill.root.innerHTML
   };
+});
+
+let renderExtendedEditor = function (basicToolbar) {
+  basicToolbar.modules.toolbar[4].push('image');
+  basicToolbar.formats.push('image');
+
+  basicToolbar.modules.imageResize = {
+    displaySize: true,
+    displayStyles: {
+      backgroundColor: 'black',
+      border: 'none',
+      color: 'white'
+    },
+    modules: [ 'Resize', 'DisplaySize', 'Toolbar' ]
+  };
+
+  let quill = renderBasicEditor(basicToolbar, '#editor-container', true);
 
   quill.getModule('toolbar').addHandler('image', () => {
     importImage(quill);
   });
-});
+
+  return quill
+}
+
+let renderBasicEditor = function (
+  basicToolbar,
+  div='#editor-container-strict',
+  allowImages=false
+) {
+  let quill = new Quill(div, basicToolbar)
+
+  if (!allowImages) {
+    quill.on('text-change', function (delta, oldDelta, source) {
+      $(`${div} img`).remove();
+    });
+  }
+
+  return quill
+};
 
 var importImage = function (textEditor) {
   const input = document.createElement('input');
