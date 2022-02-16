fc7df076de49f0522edf88d4f0714abbf3c9c191
Jessica Été <kohrVid@zoho.com>
2022-02-16 15:19:45 +0100

    Replace sass-rails with dartsass-rails
    sass-rails depends on libsass which is deprecated.[1] I think this
    explains the issues I've had building the app in Heroku today.
    
    [1] https://sass-lang.com/blog/libsass-is-deprecated

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const fileLoader = environment.loaders.get('file')
-fileLoader.exclude = /node_modules[\\/]quill/
-
-const svgLoader = {
-  test: /\.svg$/,
-  loader: 'svg-inline-loader'
-}
-
-environment.loaders.prepend('svg', svgLoader)
-
 module.exports = environment
